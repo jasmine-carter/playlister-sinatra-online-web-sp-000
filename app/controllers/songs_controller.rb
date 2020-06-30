65270498bd1a0d1795dedb74c32c7eae19899e5c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-06-30 23:03:59 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,7 +43,6 @@ class SongsController < ApplicationController
   post '/songs/:slug' do
     #binding.pry
     @song = Song.find_by_slug(params[:slug])
-    binding.pry
     if !params[:song].keys.include?
     end
     flash[:message] = "Successfully updated song."
